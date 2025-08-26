06387342ae29dc15777ab7db58d92463a4d74c78
thoufeeq20005 <thoufeeqkhan07@gmail.com>
2025-08-26 15:27:06 +0530

    Add TestController to handle home route for inventory management system

---
+++ springapp/src/main/java/com/example/inventory/controller/TestController.java
@@ -0,0 +1,13 @@
+package com.example.inventory.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class TestController {
+
+    @GetMapping("/")
+    public String home() {
+        return " Inventory Management System Backend is running!";
+    }
+}
